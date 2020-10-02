2c9ca3fcc0f20840451d212cee499e3203d58f1a
LiamBentinck <50850141+LiamBentinck@users.noreply.github.com>
2020-10-02 16:58:10 -0700

    Update harrypotterrunner.java

--- src/_03_harry_potter/harrypotterrunner.java
+++ src/_03_harry_potter/harrypotterrunner.java
@@ -3,7 +3,10 @@ package _03_harry_potter;
 public class harrypotterrunner {
 public static void main(String[] args) {
 	HarryPotter potter = new HarryPotter();
-	potter.makeInvisible();
-	
+	potter.makeInvisible(true);
+	potter.spyOnSnape();
+	potter.makeInvisible(false);
+	String spell = "pew pewpewpewpew";
+	potter.castSpell(spell);
 }
 }
