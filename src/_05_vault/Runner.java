5dc736f854a8f143075955bc3ec9cbf075d49885
LiamBentinck <50850141+LiamBentinck@users.noreply.github.com>
2020-11-27 16:54:43 -0800

    finished vault

--- src/_05_vault/Runner.java
+++ src/_05_vault/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 public static void main(String[] args) {
 	Vault v = new Vault();
 	JamesBond j = new JamesBond();
-	
-	
+	int b = j.findCode(v);
+	System.out.println("This is the password "+b);
 }
 }
