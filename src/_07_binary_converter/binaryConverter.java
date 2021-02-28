a47d686c5eef30db6dec7ac18b5f66145138f76b
LiamBentinck <50850141+LiamBentinck@users.noreply.github.com>
2021-02-27 19:28:53 -0800

    sTARTED BINAARY CONVERTER

---
+++ src/_07_binary_converter/binaryConverter.java
@@ -0,0 +1,19 @@
+package _07_binary_converter;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class binaryConverter {
+void setup() {
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	JPanel panel = new JPanel();
+	JTextField binary = new JTextField();
+	panel.add(binary);
+	frame.add(panel);
+	
+	
+	
+}
+}
